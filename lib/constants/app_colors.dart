f7f774650c410c3e210d824c6d302007e5d1f3d0
xs2bas <xs2bas@gmail.com>
2022-04-12 16:08:43 +0200

    First commit

--- lib/constants/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color yellow = Color.fromARGB(255, 194, 143, 45);
-  static const Color background = Color.fromARGB(255, 33, 33, 33);
-}
